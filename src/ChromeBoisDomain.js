9efce28eeb0c85fa7f7af6f6b0afba2a06ec1f82
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-02-16 14:36:50 -0500

    commit tree 5a3663c62ad656e7c161fbdfa99230ea49b365a9

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -2,19 +2,37 @@ import React, { Component } from 'react';
 import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js'
 
 
-export default class ChromeBoisDomain extends Component {
-  
-  handleMouseMove = (event) => {
-    /* TODO: This method should capture the `x` and `y` coordinates of the mouse
-     * from the event and use them to invoke the `drawChromeBoiAtCoords`
-     * function that has been provided and is already imported
-     * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
-     */
+export default class ChromeBoisDomain extends Component {  
+  handleMouseMove(event) {
+    drawChromeBoiAtCoords(event.clientX, event.clientY)
   }
   
+  handleKeyPress(event){
+    if(event.key === "a"){
+      resize("+")
+    }else if (event.key === "s"){
+      resize("-")
+    }
+
+    }  
+    
+
+    // <div>
+    //    <drawChromeBoiAtCoords />
+    // </div>
+
+    /* TODO: This method should capture the `x` and `y` coordinates of the mouse
+      from the event and use them to invoke the `drawChromeBoiAtCoords`
+      function that has been provided and is already imported
+      (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
+     */
+  
+
+ 
+  
   /* TODO: Create an event handler which, when fired, invokes the provided
-   * `toggleCycling` function with no arguments. Don't forget the click event
-   * listener that should fire it!
+   `toggleCycling` function with no arguments. Don't forget the click event
+    listener that should fire it!
    */
    
   /* TODO: Add an event listener to the `<canvas>` element to capture when a key
@@ -27,7 +45,10 @@ export default class ChromeBoisDomain extends Component {
   render() {
     return (
       <canvas 
+        onClick={toggleCycling}
         onMouseMove={this.handleMouseMove}
+        onKeyPress={this.handleKeyPress} 
+        // onKeyPress={resize}
         width='900'
         height='600'
         tabIndex="0">
@@ -35,3 +56,8 @@ export default class ChromeBoisDomain extends Component {
     )
   }
 }
+
+// - Add an event listener to the `<canvas>` element to capture when a key is pressed. When a key is pressed, an event handler should invoke the provided `resize` function with a single argument of either '+' or '-':
+//   - If the key pressed was 'a', then it should invoke `resize` and pass in '+'.
+//   - If the key pressed was 's', then it should invoke `resize` and pass in '-'.
+//   - You'll only be able to register a Keyboard event if the canvas is in focus. So on load of the page, either click the canvas for press the tab key to test out this feature.
